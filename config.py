4529c5e3f490f48d3319fba3263a475999f54c0f
johnpauljpc <jpcwork081@gmail.com>
2022-08-10 12:06:14 +0100

    editted

--- config.py
+++ config.py
@@ -7,9 +7,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = True
 
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 # Connect to the database
 
-
+#SQLALC
 #  DATABASE URL IMPLEMENTED
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:07061651502@localhost:5432/fyyur'
+#SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:07061651502@localhost:5432/fyyur'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
